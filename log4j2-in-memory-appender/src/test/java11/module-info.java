651379689e5d1f5d5c90640ae5815c16f5efdada
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-05 17:31:48 +0100

    JPMS module-info.java

---
+++ log4j2-in-memory-appender/src/test/java11/module-info.java
@@ -0,0 +1,7 @@
+open module com.github.nhojpatrick.log4j2.appenders.inmemory.tests {
+    requires com.github.nhojpatrick.log4j2.appenders.inmemory;
+    requires org.apache.logging.log4j;
+    requires org.slf4j;
+    requires transitive org.junit.jupiter.api;
+    requires transitive org.junit.jupiter.engine;
+}
