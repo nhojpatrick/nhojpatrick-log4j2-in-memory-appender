6278b6c5a11ec5ca8f752a4130f3612a16a046af
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-09-26 23:55:20 +0100

    Enrich log capture with log event time in millis

--- log4j2-in-memory-appender/src/main/java/com/github/nhojpatrick/log4j2/appenders/inmemory/LogStatement.java
+++ log4j2-in-memory-appender/src/main/java/com/github/nhojpatrick/log4j2/appenders/inmemory/LogStatement.java
@@ -4,11 +4,14 @@ public class LogStatement {
 
     private final String level;
     private final String message;
+    private final long timeMillis;
 
     public LogStatement(final String level,
-                        final String message) {
+                        final String message,
+                        final long timeMillis) {
         this.level = level;
         this.message = message;
+        this.timeMillis = timeMillis;
     }
 
     public String getLevel() {
@@ -17,6 +20,10 @@ public class LogStatement {
 
     public String getMessage() {
         return this.message;
+    }
+
+    public long getTimeMillis() {
+        return this.timeMillis;
     }
 
 }
